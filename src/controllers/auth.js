c406ddf8eb9384f74e75d51b7c721036305f571d
SUNNY <sunny.su.dev@gmail.com>
2023-01-11 17:38:11 +1100

    realtime chat app backend done

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,31 +2,6 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const getAllPokemons = async (req, res) => {
-  const pokemons = await Pokemon.find()
-    .sort({ order: 1 })
-    .populate({
-      path: "types",
-      populate: {
-        path: "type",
-        select: { name: 1, color: 1, _id: 0 },
-      },
-    })
-    .populate({
-      path: "evolution_chain",
-      select: { name: 1, imgSrc: 1, order: 1, _id: 0 },
-    })
-    .exec();
-  if (!pokemons) {
-    return res.status(404).json("pokemons have not been founded");
-  }
-  pokemons.reduce((prev, curr) => {
-    curr.types = curr.types.sort((prev, curr) => prev.slot - curr.slot);
-    return [...prev, curr];
-  }, []);
-  return res.json(pokemons);
-};
-
 const register = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
@@ -77,27 +52,6 @@ const login = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-};
-
-const addEvolutionChainToPokemon = async (req, res) => {
-  const { name } = req.params;
-  const { evolutionChain } = req.body;
-  const pokemon = await Pokemon.findOne({ name }).exec();
-  if (!pokemon) {
-    return res.status(404).json("pokemon has not been founded");
-  }
-  for (let i = 0; i < evolutionChain.length; i += 1) {
-    let target = await Pokemon.findOne({ name: evolutionChain[i] }).exec();
-    if (!target) continue;
-    pokemon.evolution_chain.addToSet(target._id);
-    await pokemon.save();
-  }
-  return res.json(pokemon);
-};
-
-const deleteAllPokemons = async (req, res) => {
-  await Pokemon.deleteMany().exec();
-  return res.json("deleted");
 };
 
 module.exports = {
