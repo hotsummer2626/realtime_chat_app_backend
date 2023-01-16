ef43b6856f587e3e7801bc9ca95d4412d0057104
SUNNY <sunny.su.dev@gmail.com>
2023-01-16 12:41:06 +1100

    update socket connection url

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const server = app.listen(PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: process.env.SOCKET_URL,
         credentials: true,
     },
 });
